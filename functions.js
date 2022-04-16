9242b82e9e83e1087b2e20bac255bc383e9b71a9
WaadAlharthi <66639713+WaadAlharthi@users.noreply.github.com>
2022-04-16 05:54:41 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,7 +3,11 @@ function getValues() {
 				const caf_time = document.getElementsById("caf_time").value;
 				const bedtime = document.getElementsById("bedtime").value;
 				
-				document.getElementById("result1").innerHTML = quantity;
+				document.getElementById("result1").innerHTML = quantity.val;
 				document.getElementById("result2").innerHTML = caf_time;
 				document.getElementById("result3").innerHTML = bedtime;
 }
+
+var X = "XX";
+
+document.getElementById("result3").innerHTML = X;
