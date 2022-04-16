338db6250e394c5e8e7e0cf7b2dbea607a6c46e0
WaadAlharthi <66639713+WaadAlharthi@users.noreply.github.com>
2022-04-16 05:16:34 -0600

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,8 @@
+function getValues() {
+				const caf_time = document.getElementsById("caf_time").value;
+				const bedtime = document.getElementsById("bedtime").value;
+				
+				document.getElementById("result1").innerHTML ="q" ;
+			
+				document.getElementById("result2").innerHTML = caf_time;
+}
