a947d96ad73fe9fb0bf46e2eb339de3232a16eaa
WaadAlharthi <66639713+WaadAlharthi@users.noreply.github.com>
2022-04-19 06:52:22 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,13 +5,26 @@ function getValues() {
 	var med_time = document.querySelector("#med_time").value;
 	var bedtime = document.querySelector("#bedtime").value;
 
+  //[ { x: min value of cof_time or med_time , y: quantity * 200 or medication dose },
+  //  { x: prev x+100, y: prev y-? },
+  // continure untill bedtime
+  //  { x: bedtime , y: depends?} ]
+  start = Math.min(med_time, cof_time));
+  
+	document.querySelector("#result1").value =start;
+  
+  
+  //var datapoints = [{x:},
+  //                  {},
+  //                  {}]
+  //chart.options.data = datapoints
 	
 	document.querySelector(".first").src = 'ezgifcom-gif-maker5.gif';
 	document.querySelector(".second").src = 'ezgifcom-gif-maker3.gif';
 	document.querySelector(".third").src = 'ezgifcom-gif-maker4.gif';
   document.querySelector(".forth").src = 'ezgifcom-gif-maker6.gif';
+  
+  
 	
 	
 }
-
-
