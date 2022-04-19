30140f3af844480b470ab4029b8a533211dd7e77
WaadAlharthi <66639713+WaadAlharthi@users.noreply.github.com>
2022-04-19 02:30:16 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -4,12 +4,7 @@ function getValues() {
 	
 	var med_time = document.querySelector("#med_time").value;
 	var bedtime = document.querySelector("#bedtime").value;
-	
-	document.querySelector("#result1").innerHTML = cof_time;			
-	document.querySelector("#result2").innerHTML = quantity;
-	
-	document.querySelector("#result3").innerHTML = med_time;
-	document.querySelector("#result4").innerHTML = bedtime;
+
 	
 	document.querySelector(".first").src = 'ezgifcom-gif-maker5.gif';
 	document.querySelector(".second").src = 'ezgifcom-gif-maker3.gif';
