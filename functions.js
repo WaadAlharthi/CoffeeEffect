8dbd635b420c0a8a9d78f6f21afc17eb00d05c7e
WaadAlharthi <66639713+WaadAlharthi@users.noreply.github.com>
2022-04-18 22:42:12 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -16,4 +16,39 @@ function getValues() {
 	document.querySelector(".third").src = 'ezgifcom-gif-maker4.gif';
   document.querySelector(".forth").src = 'ezgifcom-gif-maker6.gif';
 
-}
+
+
+		var chart = new CanvasJS.Chart("chartContainer",
+		{
+			title:{
+				text: "Spline Area Chart"
+			},    		
+			data: [
+			{        
+				type: "splineArea",
+				dataPoints: [
+				{x: new Date(1992,0), y: 2506000},     
+				{x: new Date(1993,0), y: 2798000},     
+				{x: new Date(1994,0), y: 3386000},     
+				{x: new Date(1995,0), y: 6944000},     
+				{x: new Date(1996,0), y: 6026000},     
+				{x: new Date(1997,0), y: 2394000},     
+				{x: new Date(1998,0), y: 1872000},     
+				{x: new Date(1999,0), y: 2140000},     
+				{x: new Date(2000,0), y: 7289000},     
+				{x: new Date(2001,0), y: 4830000},     
+				{x: new Date(2002,0), y: 2009000},     
+				{x: new Date(2003,0), y: 2840000},     
+				{x: new Date(2004,0), y: 2396000},     
+				{x: new Date(2005,0), y: 1613000},     
+				{x: new Date(2006,0), y: 2821000},     
+				{x: new Date(2007,0), y: 2000000},     
+				{x: new Date(2008,0), y: 1397000}    
+				]
+			}             
+			]
+		});
+
+	chart.render();
+	}
+		
